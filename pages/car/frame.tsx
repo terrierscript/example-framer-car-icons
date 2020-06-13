f4f8d78025840edb91db3e4815a1f543c9187767
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-06-13 19:47:42 +0900

    add

--- pages/car/frame.tsx
+++ pages/car/frame.tsx
@@ -1,16 +1,40 @@
 import Head from "next/head"
 import { Frame } from "framer"
-import { motion } from "framer-motion"
+import { motion, useMotionValue } from "framer-motion"
 import { randomCar } from "./cars"
 import { useRef, useEffect, useState } from "react"
 
-const Car = ({ frameRef }) => {
-  const car = randomCar()
+const Car = ({ x, y, frameRef, size }) => {
+  const [car] = useState(randomCar())
+  console.log(x, y)
+  const xx = useMotionValue(x)
+  const yy = useMotionValue(y)
+  console.log(xx, yy)
   return (
-    <Frame style={{ fontSize: "20vh",
-    }}
-    drag={true}
-    dragConstraints={frameRef}
+    <Frame
+      width={size}
+      height={size}
+      style={{
+        fontSize: "20vh",
+        background: "transparent",
+        // background: "red",
+        // x: xx,
+        // y: yy,
+      }}
+      x={x}
+      y={y}
+      drag={true}
+      dragConstraints={frameRef}
+      dragElastic={1}
+      animate={{ x: 0 }}
+      transition={{ type: "spring", damping: 0.1 }}
+      dragMomentum={true}
+      dragTransition={{
+        min: Infinity,
+        max: Infinity,
+        bounceStiffness: 10000,
+        bounceDamping: 0,
+      }}
     >
       {car}
     </Frame>
@@ -19,13 +43,38 @@ const Car = ({ frameRef }) => {
 const Home = () => {
   const ref = useRef(null)
   const [ready, setReady] = useState(false)
+  const [cars, setCars] = useState([])
   useEffect(() => {
     setReady(!!ref.current)
   }, [ref.current])
+
+  const onTap = (e, info) => {
+    console.log("tap", e, info, ref.current)
+    setCars((cars) => {
+      return [
+        ...cars,
+        {
+          x: info.point.x - 10,
+          y: info.point.y - 10,
+        },
+      ]
+    })
+  }
+
   return (
-      <Frame width="90vw" height="90vh" background={"#ccc"} ref={ref} center>
-        <Car frameRef={ref} />
-      </Frame>
+    <Frame
+      width="90vw"
+      height="90vh"
+      background={"#ccc"}
+      overflow="hidden"
+      ref={ref}
+      // center
+      onTap={onTap}
+    >
+      {cars.map((car, i) => (
+        <Car key={i} {...car} frameRef={ref} size={64} />
+      ))}
+    </Frame>
   )
 }
 
